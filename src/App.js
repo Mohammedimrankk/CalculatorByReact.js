4463f6c9fa8a8d5aeecd04057850e50840122503
MOHAMMED IMRAN <135793820+Mohammedimrankk@users.noreply.github.com>
2024-06-10 00:44:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from 'react';
+import { evaluate } from 'mathjs';
 function App() {
+  const [value,setValue]=useState("")
+  const handleButtonClick=(e)=>{
+    setValue(value+e.target.value)
+  }
+  const handleEqualClick=()=>{
+    try{
+  const result=evaluate(value);
+  setValue(result.toString())    
+    }
+    catch(error){
+      setValue("Error")
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+         <div className="calculator">
+            <form action=''>
+              <div className='display'>
+              <input type='text' value={value}/>
+              </div>
+              <div>
+                <input type='button' value='AC' onClick={e=>setValue('')}/>
+                <input type='button' value='DE' onClick={e=>setValue(value.slice(0,-1))}/>
+                <input type='button' value='.'  onClick={handleButtonClick}/>
+                <input type='button' value='/'  onClick={handleButtonClick}/>
+              </div>
+              <div>
+                <input type='button' value='7' onClick={handleButtonClick}/>
+                <input type='button' value='8' onClick={handleButtonClick}/>
+                <input type='button' value='9' onClick={handleButtonClick}/>
+                <input type='button' value='*' onClick={handleButtonClick}/>
+              </div>
+              <div>
+                <input type='button' value='4' onClick={handleButtonClick}/>
+                <input type='button' value='5' onClick={handleButtonClick}/>
+                <input type='button' value='6' onClick={handleButtonClick}/>
+                <input type='button' value='+' onClick={handleButtonClick}/>
+              </div>
+              <div>
+                <input type='button' value='1' onClick={handleButtonClick}/>
+                <input type='button' value='2' onClick={handleButtonClick}/>
+                <input type='button' value='3' onClick={handleButtonClick}/>
+                <input type='button' value='-' onClick={handleButtonClick}/>
+              </div>
+              <div>
+                <input type='button' value='0' onClick={handleButtonClick}/>
+                <input type='button' value='00' onClick={handleButtonClick}/>
+                <input type='button' value='=' className="equal" onClick={handleEqualClick}/>
+              </div>
+            </form>   
+         </div>
     </div>
   );
 }
